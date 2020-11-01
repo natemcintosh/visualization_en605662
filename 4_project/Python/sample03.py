7459897d6f95adb0db5364858ac6e8e72911455d
natemcintosh <natemcintosh@gmail.com>
2020-10-31 20:41:09 -0400

    update

--- 4_project/Python/sample03.py
+++ 4_project/Python/sample03.py
@@ -1,4 +1,7 @@
+from collections import namedtuple
 import os
+from typing import Tuple
+import re
 
 import pandas as pd
 import plotly.express as px
@@ -6,29 +9,58 @@ import plotly.express as px
 
 def main():
     this_dir = os.path.dirname(__file__)
-    file = os.path.join(this_dir, "../Sample03/realestate.csv")
-    df = pd.read_csv(file)
-    new_names = [
-        "crime_rate",
-        "proportion_large_lots",
-        "proportion_industry",
-        "on_river",
-        "nox_concentration",
-        "avg_rooms",
-        "proportion_prior_1940",
-        "employment_center_dist",
-        "highway_access",
-        "tax",
-        "pupil_teacher_ratio",
-        "proportion_blacks",
-        "pct_lower_status",
-        "med_value_thou",
-    ]
-    df.rename(columns=dict(zip(df.columns, new_names)), inplace=True)
+    file = os.path.join(this_dir, "../Sample03/covid_impact_on_airport_traffic.csv")
+    df = pd.read_csv(file, parse_dates=[1])
+    df.columns = df.columns.str.lower()
+    obj_to_category(df)
+    # Can't animate with datetime objects, so convert to day of year
+    df["day_of_year"] = df.date.dt.dayofyear
 
-    fig = px.scatter_matrix(df, dimensions=new_names)
+    # Get the lat/lon out of the centroid column
+    ll = [get_lat_lon(p) for p in df.centroid.astype("string")]
+    df["lat"], df["lon"] = zip(*ll)
+
+    # Create the plotly figure
+    fig = px.scatter_geo(
+        df,
+        lat="lat",
+        lon="lon",
+        hover_name="airportname",
+        size="percentofbaseline",
+        animation_frame="day_of_year",
+        projection="natural earth",
+    )
+    # Save to file
     fig.write_html(os.path.join(this_dir, "../Sample03/Index03.html"))
+
     return df
+
+
+def obj_to_category(df: pd.DataFrame):
+    """
+    Find all the non-numeric columns that have nunique/n <= 0.10 and convert them to 
+    categories
+    """
+    str_col_names = [c for c in df.columns if pd.api.types.is_object_dtype(df[c])]
+
+    cols_under_threshold = [
+        c for c in str_col_names if df[c].nunique() / len(df) <= 0.10
+    ]
+
+    for c in cols_under_threshold:
+        df[c] = df[c].astype("category")
+
+
+LatLon = namedtuple("LatLon", "lat, lon")
+
+
+def get_lat_lon(s: str) -> LatLon:
+    """
+    Take a single row from the `centroid` column and get the lat/lon out of it
+    """
+    lon = float(re.findall(r"POINT\((-?\d+\.\d+)\s", s)[0])
+    lat = float(re.findall(r"\s(-?\d+\.\d+)\)", s)[0])
+    return LatLon(lat, lon)
 
 
 if __name__ == "__main__":
